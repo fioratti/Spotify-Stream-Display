b2479f352ea995cd8cf2f9aaa35a44031b33feb3
AlecMangkharakhili <alecmang097@gmail.com>
2018-10-24 20:33:32 -0500

    Successfully retrieved music data

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ import spotipy
 import spotipy.util as util
 from env import *
 
-token = util.prompt_for_user_token(SPOTIFY_USERNAME, scope='user-read-currently-playing', client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_SECRET_ID, redirect_uri=SPOTIFY_REDIRECT_URI)
+token = util.prompt_for_user_token(SPOTIFY_USERNAME, scope='user-read-playback-state', client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_SECRET_ID, redirect_uri=SPOTIFY_REDIRECT_URI)
 
 if token:
     sp = spotipy.Spotify(auth=token)
 
+    # RETRIEVES SONG NAME
+    print(sp.currently_playing()['item']['name'])
+
+    # RETRIEVES ARTIST NAME
+    print(sp.currently_playing()['item']['artists'][0]['name'])
+
+    # RETRIEVES ALBUM NAME
+    print(sp.currently_playing()['item']['album']['name'])
 
 else:
     print('Cannot Retrieve Token For ' + SPOTIFY_USERNAME)
